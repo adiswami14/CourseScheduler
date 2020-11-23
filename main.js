992eb2cf62ee345426f87d0b6bf71406cf01296e
adiswami14 <adithya1431@gmail.com>
2020-11-23 00:30:10 -0500

    Get text fields to interact with course values

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import Course from './modules/course.js'
 import Schedule from './modules/schedule.js'
 
-
-let currCourse = new Course("CS126", "Woodley", new Date());
-currCourse.output();
+function outputCourse() {
+    let courseName = document.getElementById('coursename').value;
+    let profName = document.getElementById('profname').value;
+    let currCourse = new Course(courseName, profName, new Date());
+    currCourse.output();
+}
+document.getElementById('submit-button').addEventListener('click', outputCourse, true);
 let schedule = new Schedule();
