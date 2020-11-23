bf83def6517dfeeb4e9fc1e4bdfc598b54f08e7d
adiswami14 <adithya1431@gmail.com>
2020-11-22 20:58:05 -0500

    Display basic table on screen

--- modules/schedule.js
+++ modules/schedule.js
@@ -1,10 +1,25 @@
 class Schedule {
     constructor() {
+        const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
         function initializeSchedule() {
             let dataTable = document.createElement("table");
-            dataTable.innerHTML = "table";
+            createTable(dataTable);
             document.body.appendChild(dataTable);
-            console.log("Schedule initialized!");
+        }
+        function createTable(table) {
+            for(let i =0; i<12;i++) {
+                let row = table.insertRow();
+                for(let j=0;j<8; j++) {
+                    let cell = row.insertCell();
+                    let text;
+                    if(j>0 && i == 0) {
+                        text = document.createTextNode(weekdays[j-1]);
+                    } else if(j == 0 && i>0) {
+                        text = document.createTextNode("Time");
+                    } else text = document.createTextNode("");
+                    cell.appendChild(text);
+                }
+            }
         }
         initializeSchedule();
     }
